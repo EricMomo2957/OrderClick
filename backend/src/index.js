7a528bdfe058b6b35c0d0b5aacc4ca616fe0ee9a
EricMomo2957 <momoe2957@gmail.com>
2026-05-30 23:56:45 +0800

    modified:   backend/src/index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,6 +16,7 @@ import manageCustomerRoutes from './routes/ManageCustomerRoutes.js';
 import eventRoutes from './routes/eventRoutes.js'; 
 import announcementRoutes from './routes/announcementRoutes.js'; 
 import adminRoutes from './routes/adminRoutes.js'; // 👈 Integrated admin utility features
+import auditRoutes from './routes/manageAuditLogRoutes.js';
 dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
@@ -103,7 +104,7 @@ app.use('/api/orders', orderRoutes);
 app.use('/api/events', eventRoutes); 
 app.use('/api/announcements', announcementRoutes); 
 app.use('/api/admin', authRoutes);
-
+app.use('/api/admin', auditRoutes);
 // Core Analytics and Metric Overview Routers
 app.use('/api/admin', adminRoutes); // 👈 This handles: /api/admin/forgot-password-requests, /api/admin/stats, etc.
 app.use('/api/admin/customers-directory', manageCustomerRoutes); // Separated cleanly to keep directory routes modular
